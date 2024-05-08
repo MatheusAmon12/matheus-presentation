2d79472eac7232eaf78bc3fe52ab9a1d1e7ec785
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 11:43:55 -0300

    feat: add responsive design to info section

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
 
   return (
     <TemplateDefault>
-      <section className='px-6'>
-        <p className='font-serif text-secondary font-bold text-[12px]'>Hello, world! Meu nome é</p>
-        <h1 className='font-sans text-white text-[24px] font-bold'>Matheus Ferreira</h1>
-        <h2 className='font-serif text-secondary font-bold text-[18px]'>Desenvolvedor Front-End</h2>
-        <p className='text-[14px] font-sans text-white'>
+      <section className='px-6 xl:px-32'>
+        <p className='font-serif text-secondary font-bold text-[0.75rem]'>Hello, world! Meu nome é</p>
+        <h1 className='font-sans text-white text-[1.5rem] font-bold xl:text-[2.375rem]'>Matheus Ferreira</h1>
+        <h2 className='font-serif text-secondary font-bold text-[1.125rem] xl:text-[1.5rem]'>Desenvolvedor Front-End</h2>
+        <p className='text-[0.875rem] font-sans text-white xl:max-w-[430px] xl:text-[1rem]'>
           Comprometido em criar experiências digitais memoráveis e funcionais, sou um Desenvolvedor Full Stack | Especializado em Front-End apaixonado pelo que faço e em converter ideias criativas em realidade no desenvolvimento.
           </p>
       </section>
