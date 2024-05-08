cc82ea609bcbe4c5c57f0f6934fccb5c2127d9ed
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 11:56:42 -0300

    fix: responsive bug resolve

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <p className='font-serif text-secondary font-bold text-[0.75rem]'>Hello, world! Meu nome é</p>
         <h1 className='font-sans text-white text-[1.5rem] font-bold xl:text-[2.375rem]'>Matheus Ferreira</h1>
         <h2 className='font-serif text-secondary font-bold text-[1.125rem] xl:text-[1.5rem]'>Desenvolvedor Front-End</h2>
-        <p className='text-[0.875rem] font-sans text-white xl:max-w-[430px] xl:text-[1rem]'>
+        <p className='text-[0.875rem] font-sans text-white max-w-[430px] xl:text-[1rem]'>
           Comprometido em criar experiências digitais memoráveis e funcionais, sou um Desenvolvedor Full Stack | Especializado em Front-End apaixonado pelo que faço e em converter ideias criativas em realidade no desenvolvimento.
           </p>
       </section>
