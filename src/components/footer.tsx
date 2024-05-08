7cfab81e853ea1e158be2b24cdedfe4b1035b20a
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 15:01:38 -0300

    feat: add links to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,13 +6,17 @@ const Footer = () => {
         <div className="flex justify-center w-full flex-col items-center py-10 bg-secondary text-white font-serif text-xs">
             <h1>Entre em contato</h1>
             <div className="flex">
-                <Button variant="link" className="gap-2 flex items-center text-white" >
-                    <Phone size={16} />
-                    <span className="text-xs block">WhatsApp</span>
+                <Button variant="link" className="gap-2 flex items-center text-white" asChild>
+                    <a href="https://wa.me/5538991500574" target="_blank">
+                        <Phone size={16} />
+                        <span className="text-xs block">WhatsApp</span>
+                    </a>
                 </Button>
-                <Button variant="link" className="gap-2 flex items-center text-white">
-                    <Mail size={16} />
-                    <span className="text-xs block">E-mail</span>
+                <Button variant="link" className="gap-2 flex items-center text-white" asChild>
+                    <a href="mailto:amonmatheus757@gmail.com" target="_blank">
+                        <Mail size={16} />
+                        <span className="text-xs block">E-mail</span>
+                    </a>
                 </Button>
             </div>
         </div>
