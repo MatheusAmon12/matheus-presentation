1fdb7c4fee5533e1a06ef5f9fca27007823255df
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 13:25:43 -0300

    style: add scale on hover to items of projects carousel

--- src/components/carousel-projects.tsx
+++ src/components/carousel-projects.tsx
@@ -11,12 +11,12 @@ const CarouselProjects = ({height, children}: CarouselProjectsProps) => {
     return ( 
         <div className="pt-10 text-center">
             {children}
-            <div className="w-full flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden pt-6 px-16 xl:justify-center">
-                <img className={`h-${height}`} src="/bookings-project.png" alt="Projeto Bookings" />
+            <div className="w-full flex gap-4 items-center overflow-x-auto [&::-webkit-scrollbar]:hidden pt-2 px-16 xl:justify-center">
+                <img className={`h-${height} hover:scale-105 cursor-pointer py-4`} src="/bookings-project.png" alt="Projeto Bookings" />
 
-                <img className={`h-${height}`} src="/delivery-project.png" alt="Projeto Delivery" />
+                <img className={`h-${height} hover:scale-105 cursor-pointer py-4`} src="/delivery-project.png" alt="Projeto Delivery" />
 
-                <img className={`h-${height}`} src="/todo-project.png" alt="Projeto To Do List" />
+                <img className={`h-${height} hover:scale-105 cursor-pointer py-4`} src="/todo-project.png" alt="Projeto To Do List" />
             </div>
 
             <Button variant="outline" className="text-secondary border-secondary hover:bg-secondary hover:text-white mt-6 space-x-2" asChild>
