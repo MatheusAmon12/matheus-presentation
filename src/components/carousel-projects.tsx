cee99b3286d1a776cfa771a23eb7f76d1564976e
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 10:58:44 -0300

    feat: add see more button to projects carousel

--- src/components/carousel-projects.tsx
+++ src/components/carousel-projects.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react";
+import { Button } from "./ui/button";
+import { ChevronRight } from "lucide-react";
 
 interface CarouselProjectsProps {
     height: string,
@@ -7,7 +9,7 @@ interface CarouselProjectsProps {
 
 const CarouselProjects = ({height, children}: CarouselProjectsProps) => {
     return ( 
-        <div className="pt-10">
+        <div className="pt-10 text-center">
             {children}
             <div className="w-full flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden pt-6 px-16">
                 <img className={`h-${height}`} src="/bookings-project.png" alt="Projeto Bookings" />
@@ -16,6 +18,13 @@ const CarouselProjects = ({height, children}: CarouselProjectsProps) => {
 
                 <img className={`h-${height}`} src="/todo-project.png" alt="Projeto To Do List" />
             </div>
+
+            <Button variant="outline" className="text-secondary border-secondary hover:bg-secondary hover:text-white mt-6 space-x-2" asChild>
+                <a href="https://www.github.com/MatheusAmon12" target="_blank">
+                    <span className="font-serif">Ver todos</span>
+                    <ChevronRight size={24} />
+                </a>
+            </Button>
 
         </div>
      );
