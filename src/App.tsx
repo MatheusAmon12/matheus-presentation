53df4b923505b122731bd63b0f867ed8d23ae28c
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 14:03:30 -0300

    refactor: receives complete prop height

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,28 @@ function App() {
   return (
     <TemplateDefault>
       <section className='px-6 xl:px-32'>
-        <p className='font-serif text-secondary font-bold text-[0.75rem]'>Hello, world! Meu nome é</p>
-        <h1 className='font-sans text-white text-[1.5rem] font-bold xl:text-[2.375rem]'>Matheus Ferreira</h1>
-        <h2 className='font-serif text-secondary font-bold text-[1.125rem] xl:text-[1.5rem]'>Desenvolvedor Front-End</h2>
+        <p className='font-serif text-secondary font-bold text-[0.75rem]'>
+          Hello, world! Meu nome é
+        </p>
+
+        <h1 className='font-sans text-white text-[1.5rem] font-bold xl:text-[2.375rem]'>
+          Matheus Ferreira
+        </h1>
+
+        <h2 className='font-serif text-secondary font-bold text-[1.125rem] xl:text-[1.5rem]'>
+          Desenvolvedor Front-End
+        </h2>
+
         <p className='text-[0.875rem] font-sans text-white max-w-[430px] xl:text-[1rem]'>
           Comprometido em criar experiências digitais memoráveis e funcionais, sou um Desenvolvedor Full Stack | Especializado em Front-End apaixonado pelo que faço e em converter ideias criativas em realidade no desenvolvimento.
-          </p>
+        </p>
       </section>
 
-      <CarouselTechnologies height='16'>
+      <CarouselTechnologies height='h-16'>
         <h2 className='text-center font-serif text-secondary font-bold'>Tecnologias e Ferramentas</h2>
       </CarouselTechnologies>
 
-      <CarouselProjects height='[377px]'>
+      <CarouselProjects height='h-[377px]'>
         <h2 className='text-center font-serif text-secondary font-bold'>Alguns Projetos</h2>
       </CarouselProjects>
     </TemplateDefault>
--- src/components/carousel-technologies.tsx
+++ src/components/carousel-technologies.tsx
@@ -10,43 +10,43 @@ const CarouselTechnologies = ({height, children}: CarouselTechnologiesProps) => 
         <div className="pt-10 xl:px-32">
             {children}
             <div className="w-full flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden pt-6 px-16 xl:grid xl:grid-rows-4 xl:grid-flow-col xl:justify-items-center xl:py-6">
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/react-original.svg" alt="React" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/react-original.svg" alt="React" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/nextjs-original.svg" alt="NextJS" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/nextjs-original.svg" alt="NextJS" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/javascript-original.svg" alt="JavaScript" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/javascript-original.svg" alt="JavaScript" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/typescript-original.svg" alt="Typescript" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/typescript-original.svg" alt="Typescript" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/nodejs-original.svg" alt="NodeJS" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/nodejs-original.svg" alt="NodeJS" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/redux-original.svg" alt="Redux" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/redux-original.svg" alt="Redux" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/html5-original.svg" alt="HTML5" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/html5-original.svg" alt="HTML5" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/css3-original.svg" alt="CSS3" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/css3-original.svg" alt="CSS3" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/figma-original.svg" alt="Figma" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/figma-original.svg" alt="Figma" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/sass-original.svg" alt="Sass" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/sass-original.svg" alt="Sass" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/npm-original.svg" alt="NPM" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/npm-original.svg" alt="NPM" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/jest-plain.svg" alt="Jest" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/jest-plain.svg" alt="Jest" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/mongodb-original.svg" alt="MongoDB" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/mongodb-original.svg" alt="MongoDB" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/postgresql-original.svg" alt="PostgreSQL" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/postgresql-original.svg" alt="PostgreSQL" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/materialui-original.svg" alt="Material UI" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/materialui-original.svg" alt="Material UI" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/bash-original.svg" alt="Git Bash" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/bash-original.svg" alt="Git Bash" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/git-original.svg" alt="GitHub" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/git-original.svg" alt="GitHub" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/vscode-original.svg" alt="VS Code" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/vscode-original.svg" alt="VS Code" />
 
-                <img className={`h-${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/tailwindcss-original-wordmark.svg" alt="Tailwind CSS" />
+                <img className={`${height} hover:scale-125 cursor-pointer hover:animate-pulse`} src="/tailwindcss-original-wordmark.svg" alt="Tailwind CSS" />
             </div>
 
         </div>
