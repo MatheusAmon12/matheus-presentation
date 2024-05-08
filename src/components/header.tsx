7a46a80650af3d1bc090112180dab683cde85ebe
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 14:56:42 -0300

    refactor: add target blank to header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,12 +9,12 @@ const Header = () => {
             </div>
             <div>
                 <Button variant="ghost" className="hover:bg-secondary" asChild>
-                    <a href="https://www.github.com/MatheusAmon12">
+                    <a href="https://www.github.com/MatheusAmon12" target="_blank">
                         <img src="/github-icon.svg" alt="GitHub" height={24} width={24} />
                     </a>
                 </Button>
                 <Button variant="ghost" className="hover:bg-secondary" asChild>
-                    <a href="https://www.linkedin.com/in/matheus-amon-dos-santos-ferreira-a52526163/">
+                    <a href="https://www.linkedin.com/in/matheus-amon-dos-santos-ferreira-a52526163/" target="_blank">
                         <LinkedinIcon color="white" size={24}/>
                     </a>
                 </Button>
