c3ce05bcf85493ed6ea8ee221f5c330efb4c4525
Matheus Amon <amonmatheus757@gmail.com>
2024-05-08 15:29:24 -0300

    perf: add images webp

--- src/components/carousel-projects.tsx
+++ src/components/carousel-projects.tsx
@@ -12,11 +12,11 @@ const CarouselProjects = ({height, children}: CarouselProjectsProps) => {
         <div className="pt-10 text-center">
             {children}
             <div className="w-full flex gap-4 items-center overflow-x-auto [&::-webkit-scrollbar]:hidden pt-6 px-16 xl:justify-center">
-                <img loading="lazy" className={`h-${height} hover:scale-105 cursor-pointer py-4`} src="/bookings-project.png" alt="Projeto Bookings" />
+                <img loading="lazy" className={`${height} hover:scale-105 cursor-pointer py-4`} src="/bookings-project.webp" alt="Projeto Bookings" />
 
-                <img loading="lazy" className={`h-${height} hover:scale-105 cursor-pointer py-4`} src="/delivery-project.png" alt="Projeto Delivery" />
+                <img loading="lazy" className={`${height} hover:scale-105 cursor-pointer py-4`} src="/delivery-project.webp" alt="Projeto Delivery" />
 
-                <img loading="lazy" className={`h-${height} hover:scale-105 cursor-pointer py-4`} src="/todo-project.png" alt="Projeto To Do List" />
+                <img loading="lazy" className={`${height} hover:scale-105 cursor-pointer py-4`} src="/todo-project.webp" alt="Projeto To Do List" />
             </div>
 
             <Button variant="outline" className="text-secondary border-secondary hover:bg-secondary hover:text-white mt-6 space-x-2" asChild>
